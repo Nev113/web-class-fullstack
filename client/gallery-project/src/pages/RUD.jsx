3a7743fabced69c1fe8e55889fb240d26ded79f0
Nev113 <nevosangkane446@gmail.com>
2023-11-10 11:45:11 +0700

    adding some details

--- client/gallery-project/src/pages/RUD.jsx
+++ client/gallery-project/src/pages/RUD.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import Navbar from "../../components/Navbar";
 
 export default function RUD(){
     const [data, setData] = useState();
